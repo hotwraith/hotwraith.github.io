42781a9074fe3d4b66533109bab5446ec7f5524c
hotwraith <hotwraith4803@gmail.com>
2025-08-11 19:21:49 +0200

    big commit for testing

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Privacy is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Privacy is cool, ' + storedName;
+}
+
+myButton.addEventListener("click", () => {
+  setUserName();
+});
---
+++ scripts/quote.js
@@ -0,0 +1,35 @@
+//import Math from 
+
+let myQuotation = document.querySelector('.top');
+let myAuthor = document.querySelector('.bottom');
+
+function setQuote(allQuotes) {
+    myQuote = allQuotes[getRandomInt(allQuotes.length-1)];
+    localStorage.setItem('quote', myQuote);
+    belleQuote = buildQuoteString(myQuote)
+    myQuotation.innerHTML = belleQuote[0];
+    myAuthor.innerHTML = belleQuote[1];
+}
+
+function buildQuoteString(quote) {
+    let formattedQuote = quote[1]
+    let bottom = " -"+ quote[0] + " " + buildDateString(quote)
+    return [formattedQuote, bottom]
+}
+
+function buildDateString(quote) {
+    let date = ""
+    if(quote[4] != 0) { date += quote[4]+"/"}
+    if(quote[3] != 0) { date += quote[3]+"/"}
+    if(quote[2] != 0) { date += quote[2]}
+    return date
+    
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+fetch('assets/quotes.json')
+    .then((response) => response.json())
+    .then((json) => setQuote(json['allQuotes']))
